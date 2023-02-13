b189d8858777e8f5e245f73c7a6b4cb999cb168a
duyhien <duyhienn175@gmail.com>
2023-02-14 03:04:29 +0700

    update

--- js/app/app.module.js
+++ js/app/app.module.js
@@ -12,30 +12,47 @@ angular.module('mainApp', [
    'galleryDetail',
    'checkoutCart',
    'login',
-   'contact', 
-   'blog',  
+   'contact',
+   'blog',
    'blogDetail',
    'about',
 ])
 
-.factory('storageServices', function () {
-   let storage = [];
-   let itemsService = {};
-   let tabName = "tabAll";
-   itemsService.insert = function (item) {
-      storage.push(item);
-   };
-   itemsService.get = function () {
-      return storage;
-   };
+   .factory('storageServices', function () {
+      let storage = [];
+      let itemsService = {};
+      let tabName = "tabAll";
+      itemsService.insert = function (item) {
+         storage.push(item);
+      };
+      itemsService.get = function () {
+         return storage;
+      };
 
-   itemsService.holdTabName = function(name) {
-      tabName = name;
-   }
+      itemsService.holdTabName = function (name) {
+         tabName = name;
+      }
 
-   itemsService.getTabName = function(){
-      return tabName;
-   }
-   
-   return itemsService;
-});
+      itemsService.getTabName = function () {
+         return tabName;
+      }
+
+      return itemsService;
+   })
+
+   .directive('fadeIn', function ($timeout) {
+      return {
+         restrict: 'A',
+         link: function ($scope, $element, attrs) {
+            $scope.$watch('selectedFormat.name', function (newValue, oldValue) {
+               if (newValue != oldValue) {
+                  $element.removeClass("ng-hide-add");
+                  $element.addClass("ng-hide-remove");
+                  $timeout(function () {
+                     $element.addClass("ng-hide-add");
+                  }, 100);
+               }
+            })
+         }
+      };
+   })
